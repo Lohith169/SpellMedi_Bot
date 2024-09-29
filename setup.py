0a8f97781e1d546ca0db58aff38bde3c48bda728
lohith169 <lohith2316@gmail.com>
2024-09-29 23:03:33 +0530

    new project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Lohith',
+    author_email= '21bec030@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
